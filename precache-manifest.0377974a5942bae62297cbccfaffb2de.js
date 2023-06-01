c0cf8cdd1a560309b6298ea582c2be333f5c59dd
Natalie Gillam <ngillam@hotmail.co.uk>
2023-06-01 20:48:29 +0100

    Updates

---
+++ precache-manifest.0377974a5942bae62297cbccfaffb2de.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "77a5108861885c0a7284",
+    "url": "/super-sticky-notes/static/css/main.e9885736.chunk.css"
+  },
+  {
+    "revision": "77a5108861885c0a7284",
+    "url": "/super-sticky-notes/static/js/main.eb96e1d7.chunk.js"
+  },
+  {
+    "revision": "6c560d8e9836de18de1a",
+    "url": "/super-sticky-notes/static/js/runtime~main.fd4c522a.js"
+  },
+  {
+    "revision": "aacd201bf72820a96c02",
+    "url": "/super-sticky-notes/static/js/2.b4876291.chunk.js"
+  },
+  {
+    "revision": "01bd645ca8ca36b2d296974c9e815582",
+    "url": "/super-sticky-notes/index.html"
+  }
+];
